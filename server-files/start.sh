b1983e4f87fdc8600d023fdea0493424e8a20ee0
Hanzerik307 <hanzerik.wyoming@gmail.com>
2025-05-15 19:21:48 -0600

    Update start.sh
    Updated format to work with my GUI python script. But will also work as normal for a pure CLI environment.

--- server-files/start.sh
+++ server-files/start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-# Install or Update game. You can comment this out if you don't want/need to update files
+
+# Install or Update game - Uncomment line below to enable the option to update steam and server files when the server starts.
 # $HOME/arma/install.sh
-# Start server
-$HOME/arma/ArmaReforgerServer -config=$HOME/arma/server.json -profile=$HOME/arma/profile -maxFPS=60
+
+# Start server - Updated format to work with my GUI version functions.
+$HOME/arma/ArmaReforgerServer \
+  -config=$HOME/arma/server.json \
+  -profile=$HOME/arma/profile \
+  -maxFPS=60 \
+  -logStats=60000 \
+  -keepNumOfLogs=5 \
+  -autoReload=30 \
+  -autoShutdown
